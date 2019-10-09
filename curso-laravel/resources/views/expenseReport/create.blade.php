99f3ec2b5e31aa06870e0705ae047ca21fe3ee52
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-09 12:48:54 -0500

    CSRF

--- curso-laravel/resources/views/expenseReport/create.blade.php
+++ curso-laravel/resources/views/expenseReport/create.blade.php
@@ -13,9 +13,10 @@
     <div class="row">
         <div class="col">
             <form action="/expense_reports" method="POST">
+                @csrf
                 <div class="form-group">
-                        <label for="title">Title:</label>
-                        <input type="text" class="form-control" id="title" name="title" placeholder="Titulo">
+                    <label for="title">Title:</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Titulo">
                 </div>
                 <button class="btn btn-primary" type="submit" >Submit</button>
             </form>
