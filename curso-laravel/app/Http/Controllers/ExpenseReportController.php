b0b9abd8d2b9ced3f962dbeb2d931287069cc479
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-09 13:14:46 -0500

    Borrando reportes

--- curso-laravel/app/Http/Controllers/ExpenseReportController.php
+++ curso-laravel/app/Http/Controllers/ExpenseReportController.php
@@ -62,7 +62,8 @@ class ExpenseReportController extends Controller
      */
     public function edit($id)
     {
-        $report = ExpenseReport::find($id);
+        $report = ExpenseReport::findOrFail($id);
+
         return view('expenseReport.edit', [
             'report' => $report
         ]);
@@ -77,7 +78,7 @@ class ExpenseReportController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $report = ExpenseReport::find($id);
+        $report = ExpenseReport::findOrFail($id);
         $report->title = $request->get('title');
         $report->save();
 
@@ -92,6 +93,24 @@ class ExpenseReportController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $report = ExpenseReport::findOrFail($id);
+        $report->delete();
+
+        return redirect('/expense_reports');
+    }
+
+    /**
+     * Confirm the specified resource from storage to delete.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function confirmDelete($id)
+    {
+        $report = ExpenseReport::findOrFail($id);
+
+        return view('expenseReport.confirmDelete', [
+            'report' => $report
+        ]);
     }
 }
--- curso-laravel/resources/views/expenseReport/confirmDelete.blade.php
+++ curso-laravel/resources/views/expenseReport/confirmDelete.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="row">
         <div class="col">
-        <h1>Edit Report {{ $report->id}}</h1>
+        <h1>Delete Report {{ $report->id}}</h1>
         </div>
     </div>
     <div class="row">
@@ -14,12 +14,8 @@
         <div class="col">
             <form action="/expense_reports/{{$report->id}}" method="POST">
                 @csrf
-                @method('put')
-                <div class="form-group">
-                    <label for="title">Title:</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Titulo" value="{{$report->title}}">
-                </div>
-                <button class="btn btn-primary" type="submit" >Submit</button>
+                @method('delete')
+                <button class="btn btn-primary" type="submit" >Delete</button>
             </form>
         </div>
     </div>
