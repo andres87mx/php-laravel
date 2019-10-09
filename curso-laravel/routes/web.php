ae2a4710683c8a5ddee6b546eff8e235210e5b63
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-09 09:41:49 -0500

    Controladores en Laravel

--- curso-laravel/routes/web.php
+++ curso-laravel/routes/web.php
@@ -11,12 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
-Route::get('/test', function () {
-    return view('test', [
-        'title' => 'Curso de laravel en Platzi!!!'
-    ]);
-});
+Route::get('/dashboard', 'DashboardController@index');
