99f188182182eb5bf923e9e37846427cf4c2adcc
Andres Ruiz <aruiz@grupooctano.com.mx>
2019-10-09 13:27:13 -0500

    Validaciones

--- curso-laravel/resources/views/expenseReport/create.blade.php
+++ curso-laravel/resources/views/expenseReport/create.blade.php
@@ -12,11 +12,20 @@
     </div>
     <div class="row">
         <div class="col">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{$error}}</li>
+                        @endforeach
+                    </ul>
+                </div> 
+            @endif
             <form action="/expense_reports" method="POST">
                 @csrf
                 <div class="form-group">
                     <label for="title">Title:</label>
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Titulo">
+                <input type="text" class="form-control" id="title" name="title" placeholder="Titulo" value="{{old('title')}}">
                 </div>
                 <button class="btn btn-primary" type="submit" >Submit</button>
             </form>
